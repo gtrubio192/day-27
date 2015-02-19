6deaa4c898f8612621fd81ef23eece644ac1bbf5
Gabe Rubio <gtrubio192@gmail.com>
2015-02-18 19:36:58 -0600

    Added field validation. Works on most validations, run into errors when checking for @ symbole with emails

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,9 +1,9 @@
 angular.module('app.controllers', [])
     .controller('FormCtrl', function($scope, $timeout){
-    $scope.name = '';
-    $scope.email = '';
-    $scope.website = '';
-    $scope.message = '';
+//    $scope.name = '';
+//    $scope.email = '';
+//    $scope.website = '';
+//    $scope.message = '';
     $scope.nameError = false;
     $scope.emailError1 = false;
     $scope.emailError2 = false;
@@ -12,31 +12,33 @@ angular.module('app.controllers', [])
     $scope.messageError = false;
 
     $scope.checkFields = function(){
+        
         if(!$scope.name)
         {
             $scope.nameError = true;
         }
-        
-        if(!$scope.message)
+        else if(!$scope.message)
         {
             $scope.messageError = true;
         }
         
-        if(!$scope.email)
-        {
-            $scope.emailError1 = true;
-        }
-        else if(!$scope.email.includes('@'))
-        {
-            $scope.emailError2 = true;
-        }
-        // must start with http://...
-        if(!$scope.website)
-        {
-            $scope.siteError = true;
-        }
-
-            
-    };
-
+        console.log('Email' + $scope.email.contains('@'));
+//        else if(!$scope.email)
+//        {
+//            $scope.emailError1 = true;
+//        }
+//        else if(!$scope.email.contains('@'))
+//        {
+//            console.log($scope.email);
+//            $scope.emailError2 = true;
+//        }
+//        
+//        else if(!$scope.website)
+//        {
+//            $scope.siteError1 = true;
+//        }
+//        else if(!$scope.website.includes('http://'))
+//        {
+//            $scope.siteError2 = true;
+    }
 });
